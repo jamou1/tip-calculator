c314522e8f37385fea486431fd75f7ea98127047
James <jamesmoulton@jamess-mbp.attlocal.net>
2016-04-11 14:19:21 -0400

    - initial commit

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -8,18 +8,84 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    //User enters meal cost and tip percent
+    @IBOutlet weak var txtMeal: UITextField!
+    @IBOutlet weak var txtTipPercent: UITextField!
+    
+    //Tip converts to $$ and total amt shown
+    @IBOutlet weak var lblTipAmt: UILabel!
+    @IBOutlet weak var lblFinalTotal: UILabel!
+    
+    var mealAmt = ""
+    var tipPct = ""
+    var totalTip : Float = 0.0
+    var totalMealCost : Float = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
+    //The Calculate Button
+    @IBAction func btnCalculate(sender: UIButton) {
+       calculateTip()
+     
+    }
 
+    //The Clear Button
+    @IBAction func btnClear(sender: UIButton) {
+        clearBox()
+    }
+    
+    func calculateTip() {
+        mealAmt = txtMeal.text!
+        tipPct = txtTipPercent.text!
+        
+        let mealAmtConversion = (mealAmt as NSString).floatValue
+        let tipPctConversion = (tipPct as NSString).floatValue
+        totalTip = mealAmtConversion * (tipPctConversion / 100)
+        totalMealCost = mealAmtConversion + totalTip
+        printTip()
+        hideKeyboards()
+        
+    }
+    
+    func printTip(){
+        //Needed to format to two decimal places
+        let formatTip = String(format: "%0.2f", totalTip)
+        let formatTotal = String(format: "%0.2f", totalMealCost)
+        
+        //Takes two decimal format and places in label area
+        lblTipAmt.text = "Tip: $\(formatTip)"
+        lblFinalTotal.text = "$\(formatTotal)"
+    }
+    
+    func clearBox(){
+        txtMeal.text = ""
+        txtTipPercent.text = ""
+        lblTipAmt.text = "Tip: $0.00"
+        lblFinalTotal.text = "$0.00"
+        hideKeyboards()
+        
+    }
+    
+    //built in func that tells keyboard to hide when user touches off
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        hideKeyboards()
+    }
+    
+    //func to make keyboard hide at correct time
+    func hideKeyboards(){
+        txtMeal.resignFirstResponder()
+        txtTipPercent.resignFirstResponder()
+        
+    }
 }
 
